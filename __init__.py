f2d87132cd268d87382d60a59cd3805b30a002c2
isham-10 <i.chaibousalhatou@ecoles-epsi.net>
2025-04-07 15:19:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,9 +8,9 @@ def exercice(valeurs):
     liste_nombres = [int(nombre) for nombre in liste_nombres]
     valeur_max = liste_nombres[0]
     for nombre in liste_nombres:
-        if nombre > valeur_max:
-            valeur_max=nombre
-    return str(valeur_max)
+        if nombre < valeur_min:
+            valeur_min=nombre
+    return str(valeur_min)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
