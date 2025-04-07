8da4f71e076de876d5bb7675749dacb8987418c5
isham-10 <i.chaibousalhatou@ecoles-epsi.net>
2025-04-07 14:41:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,5 +11,16 @@ def exercice(valeurs):
         resultat = resultat + n
     return str(resultat)
 
+@app.route('/<path:valeurs>')
+def exercice(valeurs):
+    liste_nombres = valeurs.split('/')
+    liste_nombres = [int(n) for n in liste_nombres]
+    max_value = liste_nombres[0]
+    for n in liste_nombres:
+        if n > max_value:
+            max_value = n
+
+    return f"La somme est : {sum(liste_nombres)}, La valeur maximale est : {max_value}"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
