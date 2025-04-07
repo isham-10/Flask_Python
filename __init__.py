3867c032230ce4a0ee6e60125e98d29b0acdeb45
isham-10 <i.chaibousalhatou@ecoles-epsi.net>
2025-04-07 10:00:38 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def generate_pyramid(n):
     pyramid = ''
-    for i in range(1, n + 1):
+    for i in range(1, (n + 1)):
         pyramid += ' ' * (n - i)
-        pyramid += ''.join(str(j) for j in range(1, i + 1))
-        pyramid += ''.join(str(j) for j in range(i - 1, 0, -1))
+        pyramid += ''.join(str(j) for j in range(1, (i + 1))
+        pyramid += ''.join(str(j) for j in range((i - 1), 0, -1))
         pyramid += '\n'
     return pyramid
 
